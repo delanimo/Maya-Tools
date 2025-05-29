57fb568568925b5d46866099a1c4aee6a903534b
delanimo <deleon8t4@yahoo.com>
2025-05-29 20:25:19 +0000

    Fixed core functionality

--- spaceSwitch_TR.py
+++ spaceSwitch_TR.py
@@ -9,29 +9,31 @@ Instructions: select the child object(s) (targets), then the parent object (targ
 last = mc.ls(sl=True, type='transform', tl=True)[0]
 mc.select(last, tgl=True)
 sel = mc.ls(sl=True, type='transform')
+mc.setKeyframe(sel, at=['translate', 'rotate'])
+ct = mc.currentTime(q=True)
+mc.currentTime((ct-1), e=True)
+mc.setKeyframe(sel, at=['translate', 'rotate'])
+mc.currentTime(ct, e=True)
 #Disable Viewport
 mc.refresh(suspend=True)
 #Query frame before current frame
-ct = mc.currentTime(q=True)
-mc.currentTime((ct-1), e=True)
 for each in sel:
     #Time to Space Switch
     src = last
     tgt = each
-    mc.setKeyframe(src, at=['translate', 'rotate'])
-    mc.currentTime(ct, e=True)
-    mc.setKeyframe(src, at=['translate', 'rotate'])
-    mc.parentConstraint(tgt, src, mo=True)
+    mc.setKeyframe(tgt, at=['translate', 'rotate'])
+    mc.parentConstraint(src, tgt, mo=True)
     #Key Parent Blend Channel
-    cnBlend = mc.listAttr(src, k=True, u=True)[-1]
-    blendAttr = src + '.' + cnBlend
+    mc.currentTime((ct), e=True)
+    cnBlend = mc.listAttr(tgt, k=True, u=True)[-1]
+    blendAttr = tgt + '.' + cnBlend
     mc.setAttr(blendAttr, 1)
     mc.setKeyframe(blendAttr)
     mc.currentTime((ct-1), e=True)
     mc.setAttr(blendAttr, 0)
     mc.setKeyframe(blendAttr)
-#Now, back to the current frame
-mc.currentTime(ct, e=True)
+    #Now, back to the current frame
+    mc.currentTime(ct, e=True)
 #Select target objects
 mc.select(sel, r=True)
 #Enable Viewport
